f7b4e8633b8837471127f3fada01513841c3ae20
Troy <troyhayes50@gmail.com>
2019-07-21 13:30:48 -0400

    Changed application name in the UI

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -114,7 +114,7 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "Volume Mixer Control";
+            this.notifyIcon1.Text = "KeyboardMixer";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
             // 
@@ -238,7 +238,7 @@
             this.MaximumSize = new System.Drawing.Size(300, 210);
             this.MinimumSize = new System.Drawing.Size(300, 210);
             this.Name = "MainForm";
-            this.Text = "Volume Mixer Control";
+            this.Text = "KeyboardMixer";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuStrip1.ResumeLayout(false);
